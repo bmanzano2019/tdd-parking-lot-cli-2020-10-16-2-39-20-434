3326e1cb5cee8467065b22427fa47dbad98b6a21
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-16 14:04:25 +0800

    [parkingboy][baron] test: add test case for parking a car, should return ticket

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -1,10 +1,21 @@
 package com.oocl.cultivation;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ParkingBoyTest {
-    @Test
-    void write_your_first_test() {
 
+    @Test
+    void should_return_parking_ticket_when_parking_given_car_to_parkingboy() {
+        // given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+
+        // when
+        ParkingTicket ticket = parkingBoy.park(car);
+
+        // then
+        Assertions.assertNotNull(ticket);
     }
+
 }
