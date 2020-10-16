4f308078643050dcbf702a5b1c3e4bc93443c3f5
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-16 15:18:29 +0800

    [parkingboy][baron] feature: support feature when given parkingLot at full capacity, when park() another car, should fail and return no ticket refactor ParkingLot to hold the carTicketMapper since it is more appropriate add ParkingLot methods: isBelowParkingCapacity(), addCar(), removeCar()

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,23 +1,20 @@
 package com.oocl.cultivation;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ParkingBoy {
-    private final Map<ParkingTicket, Car> carTicketMapper = new HashMap<>();
+    private ParkingLot parkingLot;
 
     public ParkingBoy(ParkingLot parkingLot) {
-
+        this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
-        ParkingTicket ticket = new ParkingTicket();
-        carTicketMapper.put(ticket, car);
-
-        return ticket;
+        if (parkingLot.isBelowParkingCapacity()) {
+            return parkingLot.addCar(car);
+        }
+        return null;
     }
 
     public Car fetchCar(ParkingTicket ticket) {
-        return carTicketMapper.remove(ticket);
+        return parkingLot.removeCar(ticket);
     }
 }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -1,8 +1,13 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
     private int parkingCapacity;
+    private int currentCarCount = 0;
     private final static int DEFAULT_PARKING_CAPACITY = 10;
+    private final Map<ParkingTicket, Car> carTicketMapper = new HashMap<>();
 
     public ParkingLot() {
         this.parkingCapacity = DEFAULT_PARKING_CAPACITY;
@@ -10,5 +15,22 @@ public class ParkingLot {
 
     public ParkingLot(int parkingCapacity) {
         this.parkingCapacity = parkingCapacity;
+    }
+
+    public boolean isBelowParkingCapacity() {
+        return currentCarCount < parkingCapacity;
+    }
+
+    public ParkingTicket addCar(Car car) {
+        ParkingTicket ticket = new ParkingTicket();
+        carTicketMapper.put(ticket, car);
+        currentCarCount++;
+
+        return ticket;
+    }
+
+    public Car removeCar(ParkingTicket ticket) {
+        currentCarCount--;
+        return carTicketMapper.remove(ticket);
     }
 }
