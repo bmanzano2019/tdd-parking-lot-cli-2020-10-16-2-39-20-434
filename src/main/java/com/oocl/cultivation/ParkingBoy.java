6a2c9337f28dafdc772902d981f5fa7b7a7c7619
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-16 14:19:06 +0800

    [parkingboy][baron] feature: support returning correct car when fetchCar() given parkingticket

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,14 +1,23 @@
 package com.oocl.cultivation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingBoy {
+    private final Map<ParkingTicket, Car> carTicketMapper = new HashMap<>();
+
     public ParkingBoy(ParkingLot parkingLot) {
+
     }
 
     public ParkingTicket park(Car car) {
-        return new ParkingTicket();
+        ParkingTicket ticket = new ParkingTicket();
+        carTicketMapper.put(ticket, car);
+
+        return ticket;
     }
 
     public Car fetchCar(ParkingTicket ticket) {
-        return new Car();
+        return carTicketMapper.get(ticket);
     }
 }
