5b69e1dd66522b58d41559f8c9d6eb3a83e251a4
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-18 10:48:40 +0800

    [parkingboy][baron] feature: support parkingboy being able to handle multiple parking lots parkingboy now parks in the next parkinglot if the first parking lot is full

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,17 +1,50 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingBoy {
-    private ParkingLot parkingLot;
+    private List<ParkingLot> groupParkingLots;
 
     public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        groupParkingLots = new ArrayList<>();
+        groupParkingLots.add(parkingLot);
+    }
+
+    public ParkingBoy(List<ParkingLot> groupParkingLots) {
+        // clone this if necessary since lists are passed by reference
+        this.groupParkingLots = groupParkingLots;
     }
 
     public ParkingTicket park(Car car) {
-        return parkingLot.addCar(car);
+        // try to refactor to lambda function later
+        RuntimeException caughtException = null;
+        for (ParkingLot parkingLot : groupParkingLots) {
+            try {
+                return parkingLot.addCar(car);
+            } catch (RuntimeException parkingException) {
+                // do nothing, scan all parking lots first
+                caughtException = parkingException;
+            }
+        }
+
+        // handle scenario if the parking lot list is NULL
+        throw caughtException;
     }
 
     public Car fetchCar(ParkingTicket ticket) {
-        return parkingLot.removeCar(ticket);
+        // try to refactor to lambda function later
+        RuntimeException caughtException = null;
+        for (ParkingLot parkingLot : groupParkingLots) {
+            try {
+                return parkingLot.removeCar(ticket);
+            } catch (RuntimeException fetchingException) {
+                // do nothing, scan all parking lots first
+                caughtException = fetchingException;
+            }
+        }
+
+        // handle scenario if the parking lot list is NULL
+        throw caughtException;
     }
 }
