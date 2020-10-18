ca5e9c7f0dfcf98e32f6d4ca04a0a44787e9f90b
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-18 17:36:39 +0800

    [parkingboy][baron] test: added tests for ParkingLotServiceManager, imported from ParkingBoyTest

--- src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotServiceManagerTest.java
@@ -6,10 +6,170 @@ import com.oocl.cultivation.exception.UnrecognizedParkingTicketException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ParkingLotServiceManagerTest {
     private static final String TEST_UNRECOGNIZED_PARKING_TICKET_MESSAGE = "Unrecognized Parking Ticket.";
     private static final String TEST_NULL_PARKING_TICKET_MESSAGE = "Please provide your parking ticket.";
     private static final String TEST_FULL_PARKING_CAPACITY_MESSAGE = "Not enough position.";
+
+    @Test
+    void should_return_parking_ticket_when_parking_given_car_to_parking_manager() {
+        // given
+        Car car = new Car();
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot());
+
+        // when
+        ParkingTicket ticket = parkingManager.park(car);
+
+        // then
+        Assertions.assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_correct_car_when_fetch_car_given_parking_ticket_to_parking_manager() {
+        // given
+        Car car = new Car();
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot());
+        ParkingTicket ticket = parkingManager.park(car);
+
+        // when
+        // then
+        Assertions.assertSame(car, parkingManager.fetchCar(ticket));
+    }
+
+    @Test
+    void should_return_two_correct_cars_when_fetch_car_given_two_parking_tickets_to_parking_manager() {
+        // given
+        Car car = new Car();
+        Car otherCar = new Car();
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot());
+        ParkingTicket ticket = parkingManager.park(car);
+        ParkingTicket otherTicket = parkingManager.park(otherCar);
+
+        // when
+        // then
+        Assertions.assertSame(car, parkingManager.fetchCar(ticket));
+        Assertions.assertSame(otherCar, parkingManager.fetchCar(otherTicket));
+    }
+
+    @Test
+    void should_return_no_car_when_fetch_car_given_wrong_ticket_to_parking_manager() {
+        // given
+        Car car = new Car();
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot());
+        ParkingTicket correctTicket = parkingManager.park(car);
+        ParkingTicket wrongTicket = new ParkingTicket();
+
+        // when
+        // then
+        Assertions.assertNotSame(correctTicket, wrongTicket);
+        Exception thrownException = Assertions
+                .assertThrows(UnrecognizedParkingTicketException.class,
+                        () -> parkingManager.fetchCar(wrongTicket));
+        Assertions.assertEquals(TEST_UNRECOGNIZED_PARKING_TICKET_MESSAGE, thrownException.getMessage());
+    }
+
+    @Test
+    void should_return_no_car_when_fetch_car_given_no_ticket_to_parking_manager() {
+        // given
+        Car car = new Car();
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot());
+        parkingManager.park(car);
+
+        // when
+        // then
+        Exception thrownException = Assertions
+                .assertThrows(NullParkingTicketException.class,
+                        () -> parkingManager.fetchCar(null));
+        Assertions.assertEquals(TEST_NULL_PARKING_TICKET_MESSAGE, thrownException.getMessage());
+    }
+
+    @Test
+    void should_return_no_car_when_fetch_car_given_already_used_ticket_to_parking_manager() {
+        // given
+        Car car = new Car();
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot());
+        ParkingTicket ticket = parkingManager.park(car);
+        parkingManager.fetchCar(ticket);
+
+        // when
+        // then
+        Exception thrownException = Assertions
+                .assertThrows(UnrecognizedParkingTicketException.class,
+                        () -> parkingManager.fetchCar(ticket));
+        Assertions.assertEquals(TEST_UNRECOGNIZED_PARKING_TICKET_MESSAGE, thrownException.getMessage());
+    }
+
+    @Test
+    void should_park_car_fail_and_return_no_ticket_when_park_car_given_parking_lot_capacity_one_and_car_already_parked() {
+        // given
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(new ParkingLot(1));
+        parkingManager.park(new Car());
+
+        // when
+        // then
+        Exception thrownException = Assertions
+                .assertThrows(FullParkingCapacityException.class,
+                        () -> parkingManager.park(new Car()));
+        Assertions.assertEquals(TEST_FULL_PARKING_CAPACITY_MESSAGE, thrownException.getMessage());
+    }
+
+    @Test
+    void should_park_car_at_second_lot_when_park_car_given_first_parking_lot_is_full() {
+        // given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> groupParkingLots = new ArrayList<>(0);
+        groupParkingLots.add(firstParkingLot);
+        groupParkingLots.add(secondParkingLot);
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(groupParkingLots);
+
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        ParkingTicket firstTicket = parkingManager.park(firstCar);
+
+        // when
+        ParkingTicket secondTicket = parkingManager.park(secondCar);
+
+        // then
+        Assertions.assertTrue(secondParkingLot.checkIfCarInParkingLotByTicket(secondTicket));
+        Assertions.assertSame(firstCar, parkingManager.fetchCar(firstTicket));
+        Assertions.assertSame(secondCar, parkingManager.fetchCar(secondTicket));
+    }
+
+    @Test
+    void should_park_car_at_third_lot_when_park_car_given_four_parking_lots_and_first_second_parking_lots_are_full() {
+        // given
+        ParkingLot firstParkingLot = new ParkingLot(1);
+        ParkingLot secondParkingLot = new ParkingLot(1);
+        ParkingLot thirdParkingLot = new ParkingLot();
+        ParkingLot fourthParkingLot = new ParkingLot();
+        List<ParkingLot> groupParkingLots = new ArrayList<>(0);
+        groupParkingLots.add(firstParkingLot);
+        groupParkingLots.add(secondParkingLot);
+        groupParkingLots.add(thirdParkingLot);
+        groupParkingLots.add(fourthParkingLot);
+        ParkingLotServiceManager parkingManager = new ParkingLotServiceManager(groupParkingLots);
+
+        Car firstCar = new Car();
+        Car secondCar = new Car();
+        Car thirdCar = new Car();
+        ParkingTicket firstTicket = parkingManager.park(firstCar);
+        ParkingTicket secondTicket = parkingManager.park(secondCar);
+
+        // when
+        ParkingTicket thirdTicket = parkingManager.park(thirdCar);
+
+        // then
+        Assertions.assertTrue(thirdParkingLot.checkIfCarInParkingLotByTicket(thirdTicket));
+        Assertions.assertSame(thirdCar, parkingManager.fetchCar(thirdTicket));
+        Assertions.assertSame(firstCar, parkingManager.fetchCar(firstTicket));
+        Assertions.assertSame(secondCar, parkingManager.fetchCar(secondTicket));
+    }
+
+
 
     @Test
     void should_two_parking_boys_have_parked_one_car_when_park_two_cars_given_parking_manager_has_two_boys_with_parking_lot() {
