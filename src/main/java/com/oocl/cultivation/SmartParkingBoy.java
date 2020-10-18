f1fa6b2ab6ee81921eb66457987a13d9010b7419
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-18 12:04:59 +0800

    [parkingboy][baron] feature: add initial smartParkingBoy class

---
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -0,0 +1,14 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class SmartParkingBoy extends ParkingBoy {
+    public SmartParkingBoy(ParkingLot parkingLot) {
+        super(parkingLot);
+    }
+
+    public SmartParkingBoy(List<ParkingLot> groupParkingLots) {
+        super(groupParkingLots);
+    }
+
+}
