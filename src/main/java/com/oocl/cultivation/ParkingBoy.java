9267695163cd416bb615f0538066f84c96bc7330
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-19 23:28:29 +0800

    [parkingboy][baron] refactor: refactor fetchCar, moved finding target parking lot to findParkingLotWhereCarIsParked() revised code to lambda

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -25,26 +25,29 @@ public class ParkingBoy {
         throw new ParkingException(ParkingExceptionMessage.FULL_PARKING_CAPACITY_MESSAGE);
     }
 
-    // TODO: refactor the parkingLot searching to another method
-    // TODO: and then have the subclasses override this function instead
     public Car fetchCar(ParkingTicket ticket) {
-        // TODO try to refactor to lambda function later
         if (ticket == null) {
             throw new ParkingException(ParkingExceptionMessage.NULL_PARKING_TICKET_MESSAGE);
         }
-
-        for (ParkingLot parkingLot : groupParkingLots) {
-            Car fetchedCar = parkingLot.removeCar(ticket);
-            if (fetchedCar != null) {
-                return fetchedCar;
-            }
+        ParkingLot targetParkingLot = findParkingLotWhereCarIsParked(ticket);
+        if (targetParkingLot != null) {
+            return targetParkingLot.removeCar(ticket);
         }
-
         throw new ParkingException(ParkingExceptionMessage.UNRECOGNIZED_PARKING_TICKET_MESSAGE);
     }
 
+    private ParkingLot findParkingLotWhereCarIsParked(ParkingTicket ticket) {
+        return groupParkingLots.stream()
+                .filter(parkingLot -> parkingLot.checkIfCarInParkingLotByTicket(ticket))
+                .findFirst()
+                .orElse(null);
+    }
+
     ParkingLot findAvailableParkingLot() {
-        return groupParkingLots.stream().filter(parkingLot -> !parkingLot.isFullCapacity()).findFirst().orElse(null);
+        return groupParkingLots.stream()
+                .filter(parkingLot -> !parkingLot.isFullCapacity())
+                .findFirst()
+                .orElse(null);
     }
 
 }
