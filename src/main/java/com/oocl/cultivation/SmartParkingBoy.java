788f8e39dd132691bebcff53a66b97c56f26a1ad
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-19 23:05:29 +0800

    [parkingboy][baron] refactor: refactor smartparkingboy to override findAvailableParkingLot() instead

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -3,6 +3,7 @@ package com.oocl.cultivation;
 import com.oocl.cultivation.exception.ParkingException;
 import com.oocl.cultivation.utils.ParkingExceptionMessage;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class SmartParkingBoy extends ParkingBoy {
@@ -15,20 +16,10 @@ public class SmartParkingBoy extends ParkingBoy {
     }
 
     @Override
-    public ParkingTicket park(Car car) {
-        ParkingLot targetParkingLot = null;
-        int largestParkingCapacity = 0;
-        for (ParkingLot parkingLot : groupParkingLots) {
-            int parkingCapacity = parkingLot.getCurrentParkingCapacity();
-            if (parkingCapacity > largestParkingCapacity) {
-                targetParkingLot = parkingLot;
-                largestParkingCapacity = parkingCapacity;
-            }
-        }
-        if (targetParkingLot != null) {
-            return targetParkingLot.addCar(car);
-        }
-
-        throw new ParkingException(ParkingExceptionMessage.FULL_PARKING_CAPACITY_MESSAGE);
+    ParkingLot findAvailableParkingLot() {
+        return groupParkingLots.stream()
+                .filter(parkingLot -> !parkingLot.isFullCapacity())
+                .max(Comparator.comparing(ParkingLot::getCurrentParkingCapacity))
+                .orElse(null);
     }
 }
